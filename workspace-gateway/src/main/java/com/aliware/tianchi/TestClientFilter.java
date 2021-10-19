8b1858f408d730ec9d0cf54ae380ce658440aaa4
harryhare <harryhare@163.com>
2021-10-19 16:30:37 +0800

    test

--- workspace-gateway/src/main/java/com/aliware/tianchi/TestClientFilter.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/TestClientFilter.java
@@ -6,6 +6,7 @@ import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.remoting.TimeoutException;
 import org.apache.dubbo.remoting.exchange.Response;
 import org.apache.dubbo.rpc.*;
+import org.apache.dubbo.rpc.support.RpcUtils;
 
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -23,14 +24,30 @@ import java.util.function.Supplier;
  */
 @Activate(group = CommonConstants.CONSUMER)
 public class TestClientFilter implements Filter, BaseFilter.Listener {
+    static long start = System.nanoTime();
+
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        Result result= invoker.invoke(invocation);
-        RpcContext.getClientAttachment().setAttachment("timeout", 10);
+        Result result = invoker.invoke(invocation);
         try {
             MyLog.println("TestClientFilter.invoke.before");
-//            Map<String, String> attachments = invocation.getAttachments();
-//            attachments.get(Constants.TIMEOUT_KEY);
+            boolean isAsync = RpcUtils.isAsync(invoker.getUrl(), invocation);
+            boolean isOneway = RpcUtils.isOneway(invoker.getUrl(), invocation);
+            System.out.println(invoker.getUrl());
+            System.out.printf("invocation attachment: %s\n", invocation.getAttachments());
+            System.out.printf("invocation attr: %s \n", invocation.getAttributes());
+            System.out.printf("rpc context: %s \n", RpcContext.getContext());
+            System.out.printf("rpc client context: %s \n", RpcContext.getClientAttachment());
+            System.out.printf("rpc server attachment: %s \n", RpcContext.getServerAttachment());
+            System.out.printf("rpc server context: %s \n", RpcContext.getServerContext());
+            System.out.printf("rpc service context: %s \n", RpcContext.getServiceContext());
+            System.out.printf("isAsync:%s\n", isAsync);
+            System.out.printf("isOneway:%s\n", isOneway);
+            System.out.println(RpcUtils.getTimeout(invocation, -1));
+
+            long cur=(System.nanoTime()-start)/1000_000_000;//0-240
+            long timeout=(240-cur)*cur/5000+100;
+            RpcContext.getClientAttachment().setAttachment("timeout", 100);
             result.get(100, TimeUnit.MILLISECONDS);
             MyLog.printf("TestClientFilter.invoke.after %s\n", result);
             return result;
