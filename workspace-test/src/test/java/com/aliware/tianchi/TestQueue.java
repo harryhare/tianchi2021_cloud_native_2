43dd8392412614ae0a70dbc54d4bb19db888c979
harryhare <harryhare@163.com>
2021-10-23 15:57:32 +0800

    use by weight

--- workspace-test/src/test/java/com/aliware/tianchi/TestQueue.java
+++ workspace-test/src/test/java/com/aliware/tianchi/TestQueue.java
@@ -2,7 +2,9 @@ package com.aliware.tianchi;
 
 import org.junit.Test;
 
+import java.util.PriorityQueue;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class TestQueue {
     @Test
@@ -16,5 +18,31 @@ public class TestQueue {
         System.out.println(q.pollFirst());
         System.out.println(q.pollFirst());
         System.out.println(q.pollFirst());
+    }
+    @Test
+    public void test2(){
+        PriorityQueue<Integer> q=new PriorityQueue<>();
+        q.add(1);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+    }
+
+    @Test
+    public void test3(){
+        PriorityBlockingQueue<Integer> q=new PriorityBlockingQueue<>();
+        q.add(1);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
     }
 }
--- workspace-test/src/test/java/com/aliware/tianchi/TestSimulate.java
+++ workspace-test/src/test/java/com/aliware/tianchi/TestSimulate.java
@@ -2,6 +2,7 @@ package com.aliware.tianchi;
 
 import org.junit.Test;
 
+import java.util.PriorityQueue;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class TestSimulate {
@@ -32,9 +33,64 @@ public class TestSimulate {
         for (int i = 0; i < 500; i++) {
             double rtt = Math.pow(index, i) * Math.pow(i, 0.5) * 0.5;
             System.out.printf("current %d, rtt %.3f\n", i, rtt);
-            if(rtt>5000){
+            if (rtt > 5000) {
                 break;
             }
         }
     }
+
+    private double get_rtt(double index, int c) {
+        ThreadLocalRandom r = ThreadLocalRandom.current();
+        double a = r.nextDouble(0.6, 1.4);
+        double b = r.nextDouble();
+        double rtt = Math.pow(index, c) * Math.pow(c, 0.5) * 0.5;
+        if (rtt > 5000) {
+            rtt = 5000;
+        }
+        return rtt;
+    }
+
+    @Test
+    public void test1() {
+        PriorityQueue<Double>[] q = new PriorityQueue[3];
+        for (int i = 0; i < 3; i++) {
+            q[i] = new PriorityQueue<>();
+        }
+        double[] index = {1.045, 1.03, 1.015};
+        int counter = 0;
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 200; i++) {
+                q[j].add(get_rtt(index[j], i));
+                counter++;
+            }
+            q[j].add(Double.MAX_VALUE);
+        }
+        double[] a = new double[3];
+        while (true) {
+            Double t = Double.MAX_VALUE;
+            int drop = -1;
+            for (int i = 0; i < 3; i++) {
+                a[i] = q[i].peek();
+                if (a[i] < t) {
+                    t = a[i];
+                    drop = i;
+                }
+            }
+            assert drop >= 0 && drop <= 2;
+            assert q[drop].poll().equals(t);
+            t += get_rtt(index[drop], q[drop].size());
+
+            //int x = ThreadLocalRandom.current().nextInt(3);
+            int x=drop;
+            q[x].add(t);
+            assert q[drop].size() < 500;
+            counter++;
+            if (t > 1000 * 120) {
+                break;
+            }
+        }
+        System.out.printf("total request: %d\n", counter);
+    }
 }
+//179697，x=drop
+//27382,x=随机
